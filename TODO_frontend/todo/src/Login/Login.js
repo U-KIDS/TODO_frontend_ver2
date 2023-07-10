bf6011d83663f48c5decefb5245f903c9a30b97a
kkkkkk0312 <njs05022@naver.com>
2023-07-10 15:47:33 +0900

    background

--- TODO_frontend/todo/src/Login/Login.js
+++ TODO_frontend/todo/src/Login/Login.js
@@ -1,11 +1,20 @@
 import styles from './Login.module.css';
+// import { createGlobalStyle } from 'styled-components';
+
+// const GlobalStyle = createGlobalStyle`
+//   body {
+//     background: #e9ecef
+//   }
+// `;
 
 function Login(){
     return(
-        <div className={styles.Div}>     
+
+
             <div className={styles.itemContainer}>
             </div>
-        </div>
+
+        
     )
 }
 
