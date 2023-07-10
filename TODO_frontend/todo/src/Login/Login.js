a127303fe6de0ca586677f0236c062272d4701cf
MEGUMMY1 <cys10902@nate.com>
2023-07-10 16:22:11 +0900

    login

--- TODO_frontend/todo/src/Login/Login.js
+++ TODO_frontend/todo/src/Login/Login.js
@@ -1,10 +1,27 @@
+import React from "react";
 import styles from './Login.module.css';
+import Input from "./input";
+import useInput from "./useInput";
+import Logo from './logo.png';
 
 function Login(){
+    const [id, setID, resetID] = useInput("");
+    const [password, setPassword, resetPassword] = useInput("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        resetID();
+        resetPassword();
+    };
+
     return(
-        <div className={styles.Div}>     
-            <div className={styles.itemContainer}>
-            </div>
+        <div className={styles.itemContainer}>
+            <img src={Logo} alt="logo" className={styles.img}/>
+            <form onSubmit={handleSubmit}>
+                <Input type="text" value={id} onChange={setID} placeholder="ID"/>
+                <Input type="password" value={password} onChange={setPassword} placeholder="Password"/>
+                <button className={styles.button} type="submit">Login</button>
+            </form>
         </div>
     )
 }
